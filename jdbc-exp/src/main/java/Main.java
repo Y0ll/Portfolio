25672493d53f9b28397f9bf609c977158552f250
Artem Letavin <91343520+Y0ll@users.noreply.github.com>
2022-06-30 19:47:25 +0300

    Add files via upload

---
+++ jdbc-exp/src/main/java/Main.java
@@ -0,0 +1,32 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import java.util.Date;
+
+import java.sql.*;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure("hibernate.cfg.xml").build();
+        Metadata metadata = new MetadataSources(registry).getMetadataBuilder().build();
+        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
+
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        Students student = session.get(Students.class, 101);
+
+        session.delete(student);
+
+
+        transaction.commit();
+        sessionFactory.close();
+    }
+}
